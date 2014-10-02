d0df0244da05761b67ad26d7bc720a81356f7fdf
Stuart <stuartskelton@users.noreply.github.com>
2014-10-02 12:44:59 +0100

    added my library init

---
+++ lib/Squeeze.pm
@@ -0,0 +1,216 @@
+package Squeeze;
+use Modern::Perl;
+use Moo;
+use namespace::clean;
+use Data::Dumper;
+use Net::Telnet;
+use Types::Standard qw( Str );
+use URI::Escape;
+
+has 'host' => (
+  is      => 'ro',
+  isa     => Str,
+  default => '192.168.1.101'
+);
+
+has 't' => (
+  is  => 'rw',
+  isa => sub {
+
+    die "$_[0] is not a Net::Telnet Object!" unless ref $_[0] eq 'Net::Telnet';
+  },
+
+);
+
+has 'player' => (
+  is  => 'rw',
+  isa => Str,
+
+);
+
+sub BUILD
+{
+  my $self = shift;
+  $self->t(
+    Net::Telnet->new(
+      Timeout => 10,
+      Port    => 9090,
+      Prompt  => '/[\n]/'
+    )
+  );
+  $self->t->open( $self->host );
+
+}
+
+sub play
+{
+  my ( $self, $playerId, $track ) = @_;
+
+  $self->runcommand( "$playerId play" );
+
+}
+
+sub stop
+{
+  my ( $self, $playerId ) = @_;
+  $self->runcommand( "$playerId stop" );
+}
+
+sub playTrack
+{
+  my ( $self, $playerId, $track ) = @_;
+  return 0 unless $track =~ /^\d+$/;
+  $self->runcommand( "$playerId playlist index $track" );
+}
+
+sub nextTrack
+{
+  my ( $self, $playerId ) = @_;
+  $self->runcommand( "$playerId playlist index +1" );
+}
+
+sub prevTrack
+{
+  my ( $self, $playerId ) = @_;
+  $self->runcommand( "$playerId playlist index -1" );
+}
+
+sub volUp
+{
+
+  my ( $self, $playerId, $change ) = @_;
+  return 0 unless $change =~ /^\d+$/;
+  $self->runcommand( "$playerId  mixer volume +$change" );
+
+}
+
+sub volDown
+{
+
+  my ( $self, $playerId, $change ) = @_;
+  return 0 unless $change =~ /^\d+$/;
+  $self->runcommand( "$playerId  mixer volume -$change" );
+
+}
+
+sub playlistLength
+{
+  my ( $self, $playerId ) = @_;
+  my @a = $self->runcommand( "$playerId  playlist tracks ?" );
+  return $self->pick_field( 3, @a );
+
+}
+
+sub currentTrackNumber
+{
+  my ( $self, $playerId ) = @_;
+  my @a = $self->runcommand( "$playerId  playlist index ?" );
+  return $self->pick_field( 3, @a );
+
+}
+
+sub currentTrack
+{
+  my ( $self, $playerId ) = @_;
+  my @a = $self->runcommand( "$playerId  playlist index ?" );
+  my $currentTrackNumber = $self->pick_field( 3, @a );
+
+  my $album =
+    $self->pick_field( 4,
+    $self->runcommand( "$playerId playlist album $currentTrackNumber ?" ) );
+  my $artist =
+    $self->pick_field( 4,
+    $self->runcommand( "$playerId playlist artist $currentTrackNumber ?" ) );
+  my $title =
+    $self->pick_field( 4,
+    $self->runcommand( "$playerId playlist title $currentTrackNumber ?" ) );
+
+  return ( $currentTrackNumber, $title, $album, $artist );
+
+}
+
+sub playlist
+{
+  my ( $self, $playerId ) = @_;
+
+  my $playlistLength     = $self->playlistLength( $playerId );
+  my $currentTrackNumber = $self->currentTrackNumber( $playerId );
+  for my $i ( 0 .. $playlistLength - 1 )
+  {
+
+    my $album =
+      $self->pick_field( 4,
+      $self->runcommand( "$playerId playlist album $i ?" ) );
+    my $artist =
+      $self->pick_field( 4,
+      $self->runcommand( "$playerId playlist artist $i ?" ) );
+    my $title =
+      $self->pick_field( 4,
+      $self->runcommand( "$playerId playlist title $i ?" ) );
+
+    my $iscurrent = ( $i == $currentTrackNumber ) ? '>' : ' ';
+    say sprintf "%s %3d - '%s' from %s by %s", $iscurrent, $i, $title, $album,
+      $artist;
+
+  }
+}
+
+sub playlists
+{
+  my ( $self ) = @_;
+  my @a = $self->runcommand( "playlists 0" );
+
+  return uri_unescape( join( ' ', @a ) );
+
+}
+
+sub playPlaylist
+{
+  my ( $self, $playerId, $playlistId ) = @_;
+  return unless defined $playlistId;
+  my @a = $self->runcommand(
+    "$playerId playlistcontrol cmd:load playlist_id:$playlistId" );
+  $self->playTrack( $playerId, 0 );
+  return uri_unescape( join( ' ', @a ) );
+
+}
+
+sub shufflePlaylist
+{
+  my ( $self, $playerId ) = @_;
+  my @a = $self->runcommand( "$playerId playlist shuffle 1" );
+
+  return uri_unescape( join( ' ', @a ) );
+
+}
+
+sub randomPlay
+{
+  my ( $self, $playerId ) = @_;
+  my @a = $self->runcommand( "$playerId randomplay tracks " );
+
+  return uri_unescape( join( ' ', @a ) );
+
+}
+ 
+# a5:41:d2:cd:cd:05 playlistcontrol cmd:load album_id:22
+
+sub pick_field
+{
+  my ( $self, $field, @output ) = @_;
+  my $count = 0;
+  my $a = uri_unescape( ( split /\s+/, $output[0] )[$field] );
+  $a = '' unless defined $a ;
+  return $a ;#uri_unescape( ( split /\s+/, $output[0] )[$field] );
+
+}
+
+sub runcommand
+{
+  my ( $self, $command ) = @_;
+  my $count = 0;
+  my @lines = $self->t->cmd( $command );
+  return @lines;
+}
+
+1;
---
+++ squeeze_remote.pl
@@ -0,0 +1,93 @@
+#!/usr/bin/perl
+    use Modern::Perl;
+    use FindBin;    # locate this script
+    use lib "$FindBin::Bin/lib";
+    use Squeeze;
+    use Data::Dumper;
+
+    my @order    = qw{e r  n p j l sp pp};
+    my $s        = Squeeze->new();
+    my $playerId = 'd0:df:9a:0f:e0:ba';
+
+    my $exit   = 1;
+    my %things = (
+      'e' => {
+        short   => 'exit',
+        command => sub { $exit = 0 }
+      },
+      'n' => {
+        short   => 'next track',
+        command => sub { $_[0]->nextTrack( $_[1] ) }
+      },
+      's' => {
+        short   => 'stop',
+        command => sub { $_[0]->stop( $_[1] ) }
+      },
+      ,
+      'p' => {
+        short   => 'previous track',
+        command => sub { $_[0]->prevTrack( $_[1] ) }
+      },
+      ,
+      'r' => {
+        short   => 'refresh',
+        command => sub { }
+      },
+      'l' => {
+        short   => 'show playlist',
+        command => sub { $_[0]->playlist( $_[1] ) }
+      },
+      'sp' => {
+        short   => 'show playlists',
+        command => sub { my $a = $_[0]->playlists( $_[1] ); say $a}
+      },
+      'pp' => {
+        short   => 'play playlists',
+        command => sub { $_[0]->playPlaylist( $_[1], $_[2] ) }
+      },
+      'shp' => {
+        short   => 'shuffle playlists',
+        command => sub { say $_[0]->shufflePlaylist( $_[1] ) }
+      },
+      'rp' => {
+        short   => 'replay playlists',
+        command => sub {  $_[0]->playTrack( $_[1], 0 ) }
+      },
+      'j' => {
+        short   => 'jump to track number in playlist',
+        command => sub { $_[0]->playTrack( $_[1], $_[2] ) }
+      },
+      'rp' => {
+        short   => 'random mix
+        ',
+        command => sub { $_[0]->randomPlay( $_[1] ) }
+      },
+      ,
+
+    );
+    while ( $exit )
+    {
+
+      say join ' ', $s->currentTrack( $playerId );
+
+      say '-' x 20, "\nCommands\n", '-' x 20;
+
+      for my $key ( @order )
+      {
+        say "$key - $things{$key}{short}";
+      }
+
+      my $a = <>;
+      my $b;
+      chomp $a;
+      if ( $a =~ /:/ )
+      {
+        ( $a, $b ) = split /:/, $a;
+      }
+
+      print "\033[2J";
+      print "\033[0;0H";    #jump to 0,0
+      next unless $things{$a};
+      $things{$a}{command}->( $s, $playerId, $b );
+
+    }
